c881ba5f46febae364c82099da42c5abe46aaa42
Allison Mackey <a.mackey693@gmail.com>
2020-06-29 13:54:10 -0700

    add Ticket.js & TicketList.js to application

--- README.md
+++ README.md
@@ -66,3 +66,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+<img src="/Users/Mackerson/Downloads/Untitled Diagram (3).png" alt="diagram">
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
+import Ticket from "./Ticket";
 
-// function App(){
-//   return (
-//     <React.Fragment>
-//       <h1>Help Queue</h1>
-//       <h3>3a</h3>
-//       <h3>Thato and Haley</h3>
-//       <p><em>Firebase entries not saving!</em></p>
-//       <hr/>
-//     </React.Fragment>
-//   );
-// }
-
-//  Below we're evaluating expressions inside curly braces: 
 
 function App(){
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment */}
-      <hr/>
+      <Header/>
+      <TicketList/>
     </React.Fragment>
   );
 }
