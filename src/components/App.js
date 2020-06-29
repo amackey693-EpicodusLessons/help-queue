0be1926596c0d90cda20dab34a29696927e05ca0
Allison Mackey <a.mackey693@gmail.com>
2020-06-29 15:30:48 -0700

    remove constants from App.js to compile without warnings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
-import Ticket from "./Ticket";
+// import Ticket from "./Ticket";
 
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header/>
