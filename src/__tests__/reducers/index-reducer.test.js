2219207dc9e7894137371dec8ba44036390b1d31
Allison Mackey <a.mackey693@gmail.com>
2020-07-06 21:41:48 -0700

    dispatch actions to tests in index-reducer-test.js

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -7,13 +7,25 @@ describe("rootReducer", () => {
   let store = createStore(rootReducer)
 
   test('Check that initial state of ticketListReducer matches root reducer', () => {
-    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, {type: null}));
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'Ryan & Aimen',
+      location: '4B', 
+      issue: 'Redux action is not working correctly.',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, action));
   });
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
-    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}));
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
-  
+
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, {type: null})).toEqual({
       masterTicketList: {},
